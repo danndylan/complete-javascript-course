c8f23c85b0b14ba1fac23ae126699bbf204b946a
Dann Dylan Del Prado <31638058+danndylan@users.noreply.github.com>
2024-03-11 17:21:21 -0400

    refactored display message function

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,35 +1,42 @@
 'use strict';
 
 let targetNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  let message = '';
 
   if (!guess) {
-    message = 'No number!';
+    displayMessage('No number!');
   } else if (guess === targetNumber) {
-    message = 'Correct Number! 🎉';
+    displayMessage('Correct Number! 🎉');
     document.querySelector('.number').textContent = targetNumber;
     document.querySelector('body').style.backgroundColor = '#808000';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
   } else if (score > 0) {
-    message = guess > targetNumber ? 'Too high! ⬆️' : 'Too low! ⬇️';
+    displayMessage(guess > targetNumber ? 'Too high! ⬆️' : 'Too low! ⬇️');
     score--;
   } else {
-    message = 'You lost the game.';
+    displayMessage('You lost the game.');
     score = 0;
   }
 
-  document.querySelector('.message').textContent = message;
   document.querySelector('.score').textContent = score;
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   targetNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
