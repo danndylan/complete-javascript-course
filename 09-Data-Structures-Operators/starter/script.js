8bcd8a474557624f1bab239b5006030d64604c56
Dann Dylan Del Prado <31638058+danndylan@users.noreply.github.com>
2024-04-10 15:03:18 -0700

    coding challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,21 @@
 'use strict';
 
 ///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+///////////////////////////////////////
 // Coding Challenge #2
 
 /* 
@@ -89,6 +104,50 @@ const game = {
   },
 };
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+console.log(`Event removed: ${gameEvents.get(64)}`);
+gameEvents.delete(64);
+
+// for (let ts = 0; ts < 90; ts += 9) {
+//   for (const [time, event] of gameEvents.entries()) {
+//     if (time < ts + 9) {
+//       console.log(`[${ts < 45 ? 'FIRST' : 'SECOND'} HALF] ${time}: ${event}`);
+//       gameEvents.delete(time);
+//     }
+//   }
+// }
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()];
+console.log(
+  `An event happened, on average, every ${
+    time.pop(time) / gameEvents.size
+  } minutes`
+);
+
+for (const [time, event] of gameEvents) {
+  console.log(`[${time <= 45 ? 'FIRST' : 'SECOND'} HALF] ${time}: ${event}`);
+}
+/**
 for (const [numGoal, scorer] of game.scored.entries())
   console.log(`Goal ${numGoal + 1}: ${scorer}`);
 
@@ -103,9 +162,6 @@ for (let [team, odd] of odds) {
   console.log(`Odd of ${teamStr}: ${odd}`);
 }
 
-// console.log((team1 < team2 && game.team1) || (team2 < team1 && game.team2));
-
-/**
 const [players1, players2] = game.players;
 
 const [gk1, ...fieldPlayers1] = players1;
@@ -176,7 +232,95 @@ const restaurant = {
   },
 };
 
-/**
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+
+///// MAPS, SETS 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'we are closed');
+
+console.log(rest.get('categories'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 24;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2, 3];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+const question = new Map([
+  ['question', 'What is the best programming language?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again'],
+]);
+
+//Converting object to map
+console.log(question);
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your answer: '));
+console.log(answer);
+console.log(question.get(answer === question.get('correct')));
+
+console.log(new Set('Dylan'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Garlic Bread');
+
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+
 //Property NAMES
 const properties = Object.keys(openingHours);
 // console.log(properties);
