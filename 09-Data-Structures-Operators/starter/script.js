968da3112b01262c1569dae30d5c8e3ea3942575
Dann Dylan Del Prado <31638058+danndylan@users.noreply.github.com>
2024-04-13 18:18:17 -0400

    section done ez strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,55 @@
 'use strict';
 
 ///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const words = text.split('\n');
+
+  for (let [i, word] of words.entries()) {
+    const [first, second] = word.trim().toLowerCase().split('_');
+    const output =
+      [first, second.replace(second[0], second[0].toUpperCase())]
+        .join('')
+        .padEnd(20) + '✅'.repeat(i + 1);
+    console.log(output);
+  }
+});
+
+*/
+///////////////////////////////////////
 // Coding Challenge #3
 
 /* 
@@ -191,10 +240,31 @@ console.log((team1 < team2 && game.team1) || (team2 < team1 && game.team2));
 
 */
 
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
 // Data needed for a later exercise
+
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [name, from, to, time] = flight.split(';');
+
+  let output = `${name.startsWith('_Delayed') ? '🔴' : ''}${name.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h') + 'm'})`;
+
+  console.log(output.padStart(60, '✈'));
+}
+
+/**
+////////////// Restaurant
 // Data needed for first part of the section
 const openingHours = {
   thu: {
@@ -210,8 +280,6 @@ const openingHours = {
     close: 24,
   },
 };
-
-////////////// Restaurant
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -238,6 +306,7 @@ const restaurant = {
   },
 };
 
+///////// STRINGS
 console.log(flights.lastIndexOf('433'));
 console.log(flights.slice(18, 31));
 
@@ -259,9 +328,10 @@ checkMiddleSeat('11B');
 checkMiddleSeat('24C');
 checkMiddleSeat('32E');
 
-console.log(flights.toUpperCase());
+// console.log(flights.toUpperCase());
 
-/**
+console.log(flights.replaceAll(';', '###'));
+
 
 const ordersSet = new Set([
   'Pasta',
