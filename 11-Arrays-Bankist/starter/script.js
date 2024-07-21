2abc21a29249d3e72e6a80da9d5ced767dfacd7d
Dann Dylan Del Prado <31638058+danndylan@users.noreply.github.com>
2024-07-20 22:04:44 -0400

    Revert "foreach, assessment codes"
    This reverts commit 4425d104076e235681f44e37ba5d699731dc8bb7.

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-/**
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -74,65 +74,3 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
-*/
-// let arr = ['a', 'b', 'c', 'd', 'e'];
-
-// SLICE
-// console.log(arr.slice(2));
-// console.log(arr.slice(2, 4));
-// console.log(arr.slice(-2));
-// console.log(arr.slice(-1));
-// console.log(arr.slice(1, -2));
-// console.log(arr.slice());
-// console.log([...arr]);
-
-//SPLICE
-// console.log(arr.splice(1));
-// arr.splice(-1);
-// console.log(arr);
-// arr.splice(1, 2);
-// console.log(arr);
-/**
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// JOIN
-console.log(letters.join(' -  '));
-
-
-const arr = [23, 11, 64];
-console.log(arr.at(-1));
-
-console.log('dylan'.at(-1));
- */
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  console.log(
-    `Movement ${i + 1}: You ${
-      movement > 0 ? 'deposited' : 'withdrew'
-    } ${Math.abs(movement)}`
-  );
-}
-console.log('---FOREACH---');
-movements.forEach(function (movement, i, arr) {
-  console.log(
-    `Movement ${i + 1}: You ${
-      movement > 0 ? 'deposited' : 'withdrew'
-    } ${Math.abs(movement)} in ${arr.length} moves`
-  );
-});
